9220cc0b47184233dd3c20971be122e975cdf74c
Dean Kaila <dean.kaila@groovnow.com>
2022-06-08 15:10:06 +1200

    show contact us message when email address does not exist

--- src/components/modals/AdminAccessModal.tsx
+++ src/components/modals/AdminAccessModal.tsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 
+import { Link, makeStyles, Typography } from "@material-ui/core";
+import Box from "@material-ui/core/Box";
 import InputAdornment from "@material-ui/core/InputAdornment";
 
+import { VerifyAdminAccessApi } from "../../services/api";
+import { ToggleUserAdminAccess } from "../../store/actions/actions";
 import appTheme from "../../styling/theme";
 import { validateEmail } from "../../utils/validation";
 import BasicModal from "../BasicModal";
 import Person from "../svg/Person";
 import { Input } from "../TextInput";
-import { VerifyAdminAccessApi } from "../../services/api";
-import { ToggleUserAdminAccess } from "../../store/actions/actions";
 
 const ENTER_EMAIL_ADDRESS = "Enter your email address to make changes";
 
@@ -19,8 +21,19 @@ interface Props {
   teamId: string;
 }
 
+const useStyles = makeStyles((theme) => ({
+  link: {
+    "&:hover": {
+      color: theme.palette.error.dark,
+    },
+  },
+}));
+
 const AdminAccessModal: React.FC<Props> = ({ open, onClose, teamId }) => {
+  const classes = useStyles();
   const [emailAddress, setEmailAddress] = useState("");
+  const [showEmailVerificationError, setShowEmailVerificationError] =
+    useState(false);
   const [emailError, setEmailError] = useState("");
   const dispatch = useDispatch();
 
@@ -37,18 +50,20 @@ const AdminAccessModal: React.FC<Props> = ({ open, onClose, teamId }) => {
         setEmailAddress("");
         onClose();
       } else {
-        setEmailError("Sorry, please try again or contact us for support");
+        setShowEmailVerificationError(true);
       }
     });
   };
 
   const onCloseClick = () => {
     setEmailAddress("");
+    setShowEmailVerificationError(false);
     onClose();
   };
 
   const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmailError("");
+    setShowEmailVerificationError(false);
     setEmailAddress(e.target.value);
   };
 
@@ -81,6 +96,28 @@ const AdminAccessModal: React.FC<Props> = ({ open, onClose, teamId }) => {
         error={emailError !== ""}
         helperText={emailError}
       />
+      <>
+        {showEmailVerificationError && (
+          <Box style={{ marginLeft: "14px", marginRight: "14px" }}>
+            <Typography variant="subtitle2" color="error" display="inline">
+              {"Sorry, please try again or "}
+            </Typography>
+            <Link href="mailto:support@groovnow.com" className={classes.link}>
+              <Typography
+                variant="subtitle2"
+                color="error"
+                display="inline"
+                style={{ fontWeight: "bold" }}
+              >
+                {"contact us"}
+              </Typography>
+            </Link>
+            <Typography variant="subtitle2" color="error" display="inline">
+              {" for support."}
+            </Typography>
+          </Box>
+        )}
+      </>
     </BasicModal>
   );
 };
