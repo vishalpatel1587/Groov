89b1e51bba9fc2c7aa8cbd0e05fc5d8a6ae0a95b
vishalpatel1587 <vishalpatel1587@gmail.com>
2022-07-08 10:39:33 +1200

    Delete src/pages directory

--- src/pages/Teams/constant.ts
+++
@@ -1,6 +0,0 @@
-import { RouteLabel, RoutPath } from '../../types/routes';
-
-export const HeaderLinks = {
-    [RouteLabel.CompanyRituals]: { name: RouteLabel, route: RoutPath.CompanyRituals },
-    [RouteLabel.TeamRituals]: { name: RouteLabel, route: RoutPath.TeamRituals }
-}
