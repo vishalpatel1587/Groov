c1eb3fcbee8589079615751cad3e26680868941c
vishalpatel1587 <vishalpatel1587@gmail.com>
2022-07-08 10:41:05 +1200

    Delete src/utils directory

--- src/utils/dateUtils.ts
+++
@@ -1,9 +0,0 @@
-import { format } from "date-fns";
-
-export const formatDate = (lastUpdateTime: Date): string => {
-  if (!lastUpdateTime) {
-    return "";
-  }
-
-  return format(new Date(lastUpdateTime), "d MMM yyyy");
-};
--- src/utils/validation.ts
+++
@@ -1,9 +0,0 @@
-export const validateName = (name: string): boolean => {
-  return name !== "" && name.length >= 1;
-};
-
-export const validateEmail = (email: string): boolean => {
-  // eslint-disable-next-line no-useless-escape
-  const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  return regex.test(email);
-};
